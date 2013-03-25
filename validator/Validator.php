ffcf59441e3998e9cbbbc65d7a8b36b1b7c4bd5f
Tomáš Lebeda <lebeda@cybergenics.cz>
2013-03-25 13:22:45 +0100

    Base list of functions without implementation

--- validator/Validator.php
+++ validator/Validator.php
@@ -2,4 +2,47 @@
 
 class Validator extends ValidatorRulesProvider {
 
+	protected function alpha($input) {
+		return TRUE;
+	}
+	
+	protected function alphaNumeric($input) {
+		return TRUE;
+	}
+	
+	protected function email($input) {
+		return TRUE;
+	}
+	
+	protected function float($input) {
+		return TRUE;
+	}
+	
+	protected function integer($input) {
+		return TRUE;
+	}
+	
+	protected function ip($input) {
+		return TRUE;
+	}
+	
+	protected function ipBase64($input) {
+		return TRUE;
+	}
+	
+	protected function maxLength($input, $maxLength = NULL) {
+		return TRUE;
+	}
+	
+	protected function maxRange($input, $maxRange = NULL) {
+		return TRUE;
+	}
+	
+	protected function minLength($input, $minLength = NULL) {
+		return TRUE;
+	}
+	
+	protected function minRange($input, $minRange = NULL) {
+		return TRUE;
+	}
 }
