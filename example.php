41e0b7cb0074a50565a7fd8da9d6d1a4085578d1
Tomáš Lebeda <lebeda@cybergenics.cz>
2013-03-25 13:19:36 +0100

    Updated example of validator

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/validator/ValidatorProvider.php';
+require_once __DIR__ . '/validator/ValidatorRulesProvider.php';
 require_once __DIR__ . '/validator/Validator.php';
 
 $validator = new Validator;
-$validator->validate(58, 'integer|minLenght[28]');
+var_dump($validator->validate(58, 'integer|minLength[28]'));
