c8c6bb024798c971c0f58a97d036f84a3a400bad
Tomáš Lebeda <lebeda@cybergenics.cz>
2013-03-27 10:34:13 +0100

    Edited example of using

--- example.php
+++ example.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once __DIR__ . '/validator/ValidatorRulesProvider.php';
 require_once __DIR__ . '/validator/Validator.php';
 
 $validator = new Validator;
-var_dump($validator->validate(58, 'integer|minLength[28]'));
+var_dump($validator->validate('65465465', 'ip'));
