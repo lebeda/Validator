4326a33c14a55fe0ea943a627a54ccdf11eada7d
lebeda <lebeda@LEBEDA.shoptet.local>
2013-04-25 16:59:49 +0200

    Changed visibility of functions to public

--- validator/Validator.php
+++ validator/Validator.php
@@ -7,21 +7,21 @@ class Validator extends ValidatorRulesProvider {
 	
 	protected $specialAplhabet = 'áäčďéëěíïňóöřšťúůüýÿžÁÄČĎÉËÍÏŇÓÖŘŠŤÚŮÜÝŸŽĚçãõâêôàÇÃÕÂÊÔÀĹĽĺľŔŕ';
 
-	protected function isAlpha($input) {
+	public function isAlpha($input) {
 		$alphaRegularExpression = '~^([a-zA-Z' . $this->specialAplhabet . ']+)$~';
 		return (bool) preg_match($alphaRegularExpression, $input);
 	}
 	
-	protected function isAlphaNumeric($input) {
+	public function isAlphaNumeric($input) {
 		$alphaNumericRegularExpression = '~^([a-zA-Z0-9' . $this->specialAplhabet . ']+)$~';
 		return (bool) preg_match($alphaNumericRegularExpression, $input);
 	}
 	
-	protected function isEmail($input) {
+	public function isEmail($input) {
 		return (bool) filter_var($input, FILTER_VALIDATE_EMAIL);
 	}
 	
-	protected function isFloat($input) {
+	public function isFloat($input) {
 		if (is_numeric($input)) {
 			$input = $this->translateCommaToDots($input);
 			if ((string) $input === (string) (float) $input) {
@@ -32,7 +32,7 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isInteger($input) {
+	public function isInteger($input) {
 		if (is_numeric($input)) {
 			if ((string) $input === (string) (int) $input) {
 				return TRUE;
@@ -42,7 +42,7 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isIp($input) {
+	public function isIp($input) {
 		if ($this->isIpv4($input)) {
 			return TRUE;
 		} elseif ($this->isIpv6($input)) {
@@ -52,15 +52,15 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isIpv4($input) {
+	public function isIpv4($input) {
 		return filter_var($input, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? TRUE : FALSE;
 	}
 	
-	protected function isIpv6($input) {
+	public function isIpv6($input) {
 		return filter_var($input, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ? TRUE : FALSE;
 	}
 	
-	protected function isMaxLength($input, $maxLength) {
+	public function isMaxLength($input, $maxLength) {
 		if (
 			$this->isInteger($maxLength)
 			&& mb_strlen($input, $this->getCharset()) <= $maxLength
@@ -71,7 +71,7 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isMaxValue($input, $maxValue) {
+	public function isMaxValue($input, $maxValue) {
 		if ($this->isInteger($maxValue)) {
 			return ((int) $maxValue >= (int) $this->translateCommaToDots($input));
 		}
@@ -79,7 +79,7 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isMinLength($input, $minLength) {
+	public function isMinLength($input, $minLength) {
 		if (
 			$this->isInteger($minLength)
 			&& mb_strlen($input, $this->getCharset()) >= $minLength
@@ -90,7 +90,7 @@ class Validator extends ValidatorRulesProvider {
 		return FALSE;
 	}
 	
-	protected function isMinValue($input, $minValue) {
+	public function isMinValue($input, $minValue) {
 		if ($this->isInteger($minValue)) {
 			return ((int) $minValue <= (int) $this->translateCommaToDots($input));
 		}
